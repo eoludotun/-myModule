f85da7d772165dd110b07681e653071971f3aae1
eoludotun <eoludotun@users.noreply.github.com>
2019-01-05 17:14:18 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-  access_key = "AKIAIFW74OQUFQLTVY2A"
-  secret_key = "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"
+
   region     = "${var.region}"
   profile    = "${var.profile}"
 }
